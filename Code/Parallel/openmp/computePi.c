6d693b1c6d669c89f024fb868e639b4e40152b40
michael-little <41924368+michael-little@users.noreply.github.com>
2018-07-31 16:36:25 -0700

    PI
    PI

---
+++ Code/Parallel/openmp/computePi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+double myFunc(double x);
+
+static int long numsteps = 100000;
+
+int main() {
+
+	double pi = 0; double time = 0;
+	double dx = 1.0/numsteps;
+	double x = 0;
+	double t_start = clock();
+	for(int i = 0; i < numsteps; i++) {
+		pi += myFunc(x) * dx;
+		x += dx;
+	}
+
+	time = (clock() - t_start) / ((double)CLOCKS_PER_SEC) * 1000;
+	printf("PI = %f, duration: %f ms\n", pi, time);
+	return 0;
+}
+
+
+double myFunc(double x) {
+	return ((4.0) / (1 + x*x));
+
+}
