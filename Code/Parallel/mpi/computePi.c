a9d588b41ba316ba0ab16614387463ec3b74ff41
michael-little <41924368+michael-little@users.noreply.github.com>
2018-07-31 13:59:56 -0700

    Compute Pi Parallel
    Compute Pi using Stampede2 parallel code

--- Code/Parallel/mpi/computePi.c
+++ Code/Parallel/mpi/computePi.c
@@ -2,23 +2,40 @@
 #include <time.h>
 
 double myFunc(double x);
+double computePi(int procIC, int numP);
 
 static int long numsteps = 100000;
 
-int main() {
+int main(int argc, char **argv) {
+	int procID, numP;
+	double pi = 0.0;
+	double time = 0.0;
 
-	double pi = 0; double time = 0;
-	double dx = 1.0/numsteps;
-	double x = 0;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size( MPI_COMM_WORLD, &numP );
+	MPI_Comm_rank( MPI_COMM_WORLD, &procID );
+
 	double t_start = clock();
+	
+	pi += computePi(procID, numP);
+
+	
+	printf("PI = %f, duration: %f ms\n", pi, time);
+	MPI_Finalize();
+	time = (clock() - t_start) / ((double)CLOCKS_PER_SEC) * 1000;
+}
+
+double computePi(int procID, int numP) {
+
+	double pi = 0.0; 
+	double dx = 1.0/numsteps * procID/numP + procID * 1.0/numsteps;
+	double x = 0;
 	for(int i = 0; i < numsteps; i++) {
 		pi += myFunc(x) * dx;
 		x += dx;
 	}
-
-	time = (clock() - t_start) / ((double)CLOCKS_PER_SEC) * 1000;
-	printf("PI = %f, duration: %f ms\n", pi, time);
-	return 0;
+	
+	return pi;
 }
 
 
